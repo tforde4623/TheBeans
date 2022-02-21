98917101a54acdb0fcfe28cbc3e61793a1cae5c3
tforde4623 <sltebr33zy@gmail.com>
2022-02-21 00:01:22 -0600

    feat(comments): fixed comments, added auto scroll to bottom

--- client/src/components/HomeFeedCard/index.js
+++ client/src/components/HomeFeedCard/index.js
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
 
+import AddCommentForm from '../ShowPost/AddCommentForm';
 import { postLike, deleteLike } from '../../store/likes';
+import { getPostComments } from '../../store/comments';
 import './homeFeedCard.css';
 
 const HomeFeedCard = ({ post, likes }) => {
   const dispatch = useDispatch();
   const [showExt, setShowExt] = useState(false);
   const currUserId = useSelector(state => state.session.user.id);
+  const comments = useSelector(state => state.comments);
   const currLikes = likes.filter(like => like.user_id === currUserId);
   const isLiked = currLikes.length > 0;
+  let messageEnd = useRef(null);
+
+  const scrollComments = () => {
+    if (messageEnd) {
+      setTimeout(() => {
+        messageEnd.current?.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+      }, 150);
+    }
+  };
 
   const changeLike = () => {
     if (!isLiked) {
@@ -22,6 +34,15 @@ const HomeFeedCard = ({ post, likes }) => {
       dispatch(deleteLike(currLikes[0].id));
     }
   };
+
+  const toggleComments = () => {
+    setShowExt(!showExt);
+    scrollComments();
+  };
+
+  useEffect(() => {
+    dispatch(getPostComments(post.id));
+  }, [dispatch, post, messageEnd]);
 
   return (
     <div className='card-container'>
@@ -52,20 +73,24 @@ const HomeFeedCard = ({ post, likes }) => {
         className={showExt ? 'card-description border-bottom' : 'card-description'}
       >{ post.description }</div>
       {!showExt && <div 
-        onClick={() => setShowExt(true)} 
+        onClick={toggleComments} 
         className="show-comments">Show {post.comments.length} Comments...</div>}
-      {showExt &&
+      {showExt &&[
       <div className='content-container'>
-        {post.comments && post.comments.map(comment => (
+        {comments && Object.values(comments).map(comment => (
           <div className='comment'>
             <strong className='bold-username'>{comment.author.username}</strong> 
             <span className='comment-content'> {comment.content}</span>
           </div>
         ))}
-      </div>
-      }
+        <div style={{ float: 'left', clear: 'both' }}
+          ref={messageEnd}>
+        </div>
+      </div>,
+      <AddCommentForm scroll={scrollComments} post={post}/>
+      ]}
       {showExt && <div 
-        onClick={() => setShowExt(false)} 
+        onClick={toggleComments} 
         className={showExt ? 'show-comments border-top' : 'show-comments'}
         >Hide...</div>}
     </div>
