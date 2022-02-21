b28e4e3af307238769172e01cfa1ea4e46b8e766
tforde4623 <sltebr33zy@gmail.com>
2022-02-21 00:24:20 -0600

    feat(chat): added requirements for socketio func

---
+++ client/src/components/ChatRoom/index.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+
+const ChatRoom = () => {
+  useEffect(() => {}, []);
+  return (
+    <div>
+            
+    </div>
+  )
+}
+
+export default ChatRoom;
