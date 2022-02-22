2ed33cbcb893f6135a533096c9c136a99e6fbb47
tforde4623 <sltebr33zy@gmail.com>
2022-02-21 21:56:03 -0600

    feat(PrivMsgs): added chatroom component

---
+++ client/src/store/rooms.js
@@ -0,0 +1,68 @@
+const LOAD_ROOMS = 'rooms/load';
+
+const loadRooms = rooms => ({
+  type: LOAD_ROOMS,
+  rooms
+});
+
+export const getRooms = user_id => async dispatch => {
+  const res = await fetch(`/api/users/${user_id}/rooms`);
+  const json = await res.json()
+
+  console.log(json)
+  if (res.ok) {
+    dispatch(loadRooms(json));
+  }
+
+
+  return json;
+};
+
+const ADD_ROOM = 'rooms/add';
+
+const addRoom = room => ({
+  type: ADD_ROOM,
+  room
+});
+
+export const postRoom = room => async dispatch => {
+  const res = await fetch('/api/chat/rooms', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(room)
+  });
+  const json = await res.json();
+
+  if (res.ok) {
+    dispatch(addRoom(json));
+  }
+
+  return json;
+};
+
+
+const initialState = {};
+
+const roomReducer = (state = initialState, action) => {
+  const newState = Object.assign({}, state);
+
+  switch(action.type) {
+    case ADD_ROOM:
+      newState[action.room.id] = action.room
+      return newState;
+
+    case LOAD_ROOMS:
+      action.rooms.forEach(room => {
+        newState[room.id] = room;
+      });
+
+      return newState;
+
+    default:
+      return state;
+  } 
+};
+
+export default roomReducer;
