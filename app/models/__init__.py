2cb38d74015b24d1e1c8852bcbdfbd880b0d3e90
tforde4623 <sltebr33zy@gmail.com>
2022-02-21 18:18:26 -0600

    feat(PrivMsgs): added files in __init__ module for imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,3 +4,5 @@ from .posts import Post
 from .comments import Comment
 from .categories import Category
 from .likes import Like
+from .rooms import Room
+from .messages import Message
--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -4,6 +4,8 @@ from .posts import seed_posts, undo_posts
 from .categories import seed_categories, undo_categories
 from .comments import seed_comments, undo_comments
 from .likes import seed_likes, undo_likes
+from .rooms import seed_rooms, undo_rooms
+from .messages import seed_msgs, undo_msgs
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -13,28 +15,31 @@ seed_commands = AppGroup('seed')
 # creates 'flask seed categories' command
 @seed_commands.command('categories')
 def seed_cats():
+    # Creates the `flask seed all` command
     seed_categories()
-
-# Creates the `flask seed all` command
 
 
 @seed_commands.command('all')
 def seed():
+    # Creates the `flask seed undo` command
+
     seed_users()
     seed_categories()
     seed_posts()
     seed_comments()
     seed_likes()
-    # Add other seed functions here
-
-# Creates the `flask seed undo` command
+    seed_rooms()
+    seed_msgs()
 
 
 @seed_commands.command('undo')
 def undo():
+    # Add other undo functions here
+
     undo_users()
     undo_posts()
     undo_categories()
     undo_comments()
     undo_likes()
-    # Add other undo functions here
+    undo_rooms()
+    undo_msgs()
